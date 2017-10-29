2093538f7a3f2e13f706467ecd464049a25e7dcc
anishiyengar <32085994+anishiyengar@users.noreply.github.com>
2017-10-28 21:17:14 -0400

    Add files via upload

---
+++ urgencyScore.py
@@ -0,0 +1,20 @@
+class uregncyScore:
+
+    def __init__(self, painVal, historyVal, type, duration):
+
+        # constructor for patient
+
+        self.painVal = painVal
+        self.historyVal = historyVal
+        self.type = type
+        self.duration = duration
+
+
+    def patientData(self, clientFile):
+
+        File_object = open(r"File_Name", "Access_Mode")
+
+
+    def classifyClient(self):
+
+
